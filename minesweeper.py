f1985cf9e306cde8e0a1939f8dc616e728f7c99c
Maria Marchenko <maria.dmit.rovna.slv@gmail.com>
2024-05-23 16:15:58 +0300

    final version

--- minesweeper.py
+++ minesweeper.py
@@ -5,7 +5,7 @@ from ctypes import windll
 
 pygame.init()
 clock = pygame.time.Clock()
-icon_bomb = pygame.image.load("bomb.png")
+icon_bomb = pygame.image.load("images/bomb.png")
 pygame.display.set_icon(icon_bomb)
 screen = pygame.display.set_mode((350, 400))
 pygame.display.set_caption("Minesweeper")
@@ -18,7 +18,7 @@ color_blue = pygame.Color(0, 0, 255)
 color_white = pygame.Color(255, 255, 255)
 color_black = pygame.Color(0, 0, 0)
 
-game_font = pygame.font.Font("UniversLTStd-BoldEx.otf", 36)
+game_font = pygame.font.Font("fonts/Agitpropc.otf", 36)
 
 # Функція для показу повідомлення
 def showMessageBox(title, text, style):
@@ -35,22 +35,22 @@ color_white = pygame.Color(255, 255, 255)
 color_black = pygame.Color(0, 0, 0)
 
 # Завантаження шрифту
-game_font = pygame.font.Font("AGCrownStyle (1).ttf", 36)
+game_font = pygame.font.Font("fonts/Agitpropc.otf", 36)
 
 # Завантаження зображень блоків
-block_image = pygame.transform.scale(pygame.image.load("block.png"), (34, 34))
-selected_block_image = pygame.transform.scale(pygame.image.load("selblock.png"), (34, 34))
-blank_block_image = pygame.transform.scale(pygame.image.load("blankblock.png"), (34, 34))
+block_image = pygame.transform.scale(pygame.image.load("images/block.png"), (34, 34))
+selected_block_image = pygame.transform.scale(pygame.image.load("images/selblock.png"), (34, 34))
+blank_block_image = pygame.transform.scale(pygame.image.load("images/blankblock.png"), (34, 34))
 
 # Завантаження зображень попереджень
-warning_images = [pygame.transform.scale(pygame.image.load(f"num{i}.png"), (34, 34)) for i in range(1, 9)]
+warning_images = [pygame.transform.scale(pygame.image.load(f"images/num{i}.png"), (34, 34)) for i in range(1, 9)]
 # Завантаження зображення вибуху
-explode_image = pygame.transform.scale(pygame.image.load("explode.png"), (34, 34))
+explode_image = pygame.transform.scale(pygame.image.load("images/explode.png"), (34, 34))
 # Завантаження зображення прапора та знака питання
-flag_image = pygame.transform.scale(pygame.image.load("flag.png"), (34, 34))
-question_image = pygame.transform.scale(pygame.image.load("question.png"), (34, 34))
+flag_image = pygame.transform.scale(pygame.image.load("images/flag.png"), (34, 34))
+question_image = pygame.transform.scale(pygame.image.load("images/question.png"), (34, 34))
 # Завантаження зображення годинника
-clock_image = pygame.transform.scale(pygame.image.load("time.png"), (34, 34))
+clock_image = pygame.transform.scale(pygame.image.load("images/time.png"), (34, 34))
 
 # Список для зберігання блоків
 blocks = []
@@ -237,3 +237,64 @@ def reset_game():
     checked_blocks = []
     remaining_blocks = 0
     last_game_time = pygame.time.get_ticks()
+
+
+while True:
+    screen.fill(color_beige)
+    screen.blit(clock_image, (20, 346))
+    screen.blit(icon_bomb, (294, 346))
+
+    remaining_bombs_text = game_font.render(str(10 - flags_used), True, color_black)
+    remaining_bombs_rect = remaining_bombs_text.get_rect()
+    remaining_bombs_rect.topleft = (240, 346)
+    screen.blit(remaining_bombs_text, remaining_bombs_rect)
+
+    if last_game_time != 0:
+        mins = (pygame.time.get_ticks() - last_game_time) / 1000 / 60
+        secs = (pygame.time.get_ticks() - last_game_time) / 1000 % 60
+    else:
+        mins = pygame.time.get_ticks() / 1000 / 60
+        secs = pygame.time.get_ticks() / 1000 % 60
+
+    mins_str = f"{int(mins):02}"
+    secs_str = f"{int(secs):02}"
+
+    timer_text = game_font.render(mins_str + ":" + secs_str, True, color_black)
+    timer_rect = timer_text.get_rect()
+    timer_rect.topleft = (60, 346)
+    screen.blit(timer_text, timer_rect)
+
+    draw_grid()
+    render_blocks()
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == MOUSEMOTION:
+            selected_block = get_hovered_block(event.pos)
+            if not is_within_bounds(event.pos):
+                selected_block = reference_block
+        elif event.type == MOUSEBUTTONUP:
+            if is_within_bounds(event.pos):
+                selected_block = get_hovered_block(event.pos)
+                if selected_block:
+                    if event.button == 1:
+                        selected_block.flag_status = get_warning_count(selected_block)
+                        if selected_block.flag_status == 0:
+                            blocks_to_check.append(selected_block)
+                            find_path(selected_block)
+                        if selected_block.is_bomb:
+                            selected_block.flag_status = 9
+                    elif event.button == 3:
+                        if selected_block.flag_status == -3:
+                            selected_block.flag_status = -2
+                        elif selected_block.flag_status == -2:
+                            selected_block.flag_status = -1
+                        elif selected_block.flag_status == -1:
+                            selected_block.flag_status = -3
+    if selected_block and selected_block.flag_status == -3:
+        screen.blit(selected_block_image, selected_block.position)
+
+    pygame.display.update()
+    clock.tick(30)
